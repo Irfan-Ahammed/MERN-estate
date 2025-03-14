569598cd3bf30b726b8cfafdb1f9b3ee683f1aad
jhon-malik <cminute82@gmail.com>
2025-03-14 11:42:14 +0530

    fix deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        secure: false,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }; 
+
+  return defineConfig({
+    server: {
+      proxy: {
+        '/api': process.env.VITE_API_URL
       },
     },
-  },
 
-  plugins: [react()],
-});
+    plugins: [vercel(), react()],
+  })
+};
